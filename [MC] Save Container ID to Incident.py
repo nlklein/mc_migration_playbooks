25f78691285948e0ce9ea5c279ec0ae07efef16f
Noah Klein <nklein@splunk.com>
2023-05-23 13:11:43 +0000

    add descriptions

--- [MC] Save Container ID to Incident.py
+++ [MC] Save Container ID to Incident.py
@@ -1,5 +1,5 @@
 """
-
+The purpose of this playbook is to add the SOAR container ID to the MC incident description and then add the MC ID to the SOAR container as an artifact.
 """
 
 
@@ -24,6 +24,10 @@ def get_description(action=None, success=None, container=None, results=None, han
     phantom.debug("get_description() called")
 
     # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    ################################################################################
+    # Get current incident description.
+    ################################################################################
 
     external_id_value = container.get("external_id", None)
 
@@ -52,6 +56,10 @@ def get_description(action=None, success=None, container=None, results=None, han
 @phantom.playbook_block()
 def add_container_id_to_description(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug("add_container_id_to_description() called")
+
+    ################################################################################
+    # Append SOAR container ID to the bottom of the description.
+    ################################################################################
 
     template = """{0}\n\nSOAR Container ID: {1}"""
 
@@ -84,6 +92,10 @@ def update_incident_description(action=None, success=None, container=None, resul
 
     # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
 
+    ################################################################################
+    # Update incident description in MC.
+    ################################################################################
+
     external_id_value = container.get("external_id", None)
     add_container_id_to_description = phantom.get_format_data(name="add_container_id_to_description")
 
@@ -113,6 +125,10 @@ def update_incident_description(action=None, success=None, container=None, resul
 @phantom.playbook_block()
 def save_mc_id_to_container(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug("save_mc_id_to_container() called")
+
+    ################################################################################
+    # Save MC incident ID to SOAR container as artifact.
+    ################################################################################
 
     external_id_value = container.get("external_id", None)
     id_value = container.get("id", None)
